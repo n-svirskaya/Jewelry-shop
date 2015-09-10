29c2596e522157b97a1304c8ae7bd540dfc5e754
Natalja <nataljasvirskaya@gmail.com>
2015-09-10 13:05:03 +0300

    add html and rename DefaultController to ShopController

---
+++ html/readme.txt
@@ -0,0 +1,6 @@
+This template is the property of www.flashmint.com. You can use this template for your personal use but the link to www.flashmint.com should be kept.
+
+Thank you for choosing us!
+
+FlashMint Team
+www.flashmint.com
--- src/ShopBundle/Controller/ShopController.php
+++ src/ShopBundle/Controller/ShopController.php
@@ -4,7 +4,7 @@ namespace ShopBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class ShopController extends Controller
 {
     public function indexAction($name)
     {
