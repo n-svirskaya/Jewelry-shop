98589c1c1721b77de54a1152d302aaef3a4c9d5d
Natalja <nataljasvirskaya@gmail.com>
2015-09-18 11:46:47 +0300

    created Personal info page

--- src/ShopBundle/Controller/UserController.php
+++ src/ShopBundle/Controller/UserController.php
@@ -3,6 +3,9 @@
 namespace ShopBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use UserBundle\Form\UserType;
+
 
 class UserController extends Controller
 {
@@ -18,8 +21,25 @@ class UserController extends Controller
 
     public function userAccountAction()
     {
-        return $this->render('ShopBundle:Users:user_account.html.twig');
+       return $this->render('ShopBundle:Users:user_account.html.twig');
     }
 
+    public function infoUserAccountAction()
+    {
+        return $this->render('ShopBundle:Users:personal_info.html.twig');
+    }
 
+    public function userEditAction(Request $request)
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(new UserType(), $user);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $this->container->get('doctrine.orm.entity_manager')->flush($user);
+            return $this->redirect($this->generateUrl('personal_info'));
+        }
+
+        return $this->render('@Shop/Users/edit_user_info.html.twig', array('form' => $form->createView()));
+    }
 }
---
+++ src/UserBundle/Form/UserType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('phone')
+            ->add('company')
+            ->add('address')
+            ->add('save', 'submit', array(
+                'attr' => array('class' => 'save'),
+            ));
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'userbundle_user';
+    }
+}
